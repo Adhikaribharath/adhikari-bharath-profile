33ac0da8d8bac26f8a153455993c431e8bb23c3a
AdhikariBharath <adhikaribharath13@gmail.com>
2020-03-21 11:11:36 +0530

    First push after editing to check deploy status in netlify

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  siteTitle: 'Gatsby Starter Resume', // <title>
+  siteTitle: 'Bharath Adhikari Profile', // <title>
   manifestName: 'Resume',
   manifestShortName: 'Landing', // max 12 characters
   manifestStartUrl: '/',
@@ -8,8 +8,8 @@ module.exports = {
   manifestDisplay: 'standalone',
   manifestIcon: 'src/assets/img/website-icon.png',
   pathPrefix: `/gatsby-starter-resume/`, // This path is subpath of your hosting https://domain/portfolio
-  firstName: 'Anubhav',
-  lastName: 'Srivastava',
+  firstName: 'Bharath Kumar',
+  lastName: 'Adhikari',
   // social
   socialLinks: [
     {
@@ -33,7 +33,5 @@ module.exports = {
       url: 'https://facebook.com/theanubhav',
     },
   ],
-  email: 'anubhav.srivastava00@gmail.com',
-  phone: '000-00000',
-  address: '305 Del Sol, Sunnyvale, CA',
-};
+  email: 'adhikaribharath13@gmail.com',
+ };
